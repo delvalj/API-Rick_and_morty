4688fb739b7af03699c5f2cc236ba63cbbf69132
Joaquin <delvaljoaquin@gmail.com>
2021-08-10 17:05:47 -0300

    ListCharacters Component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <h1>Rick and Morty</h1>
+  <ListCharacters></ListCharacters>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListCharacters from "@/components/ListCharacters";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      ListCharacters
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
